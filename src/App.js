545e041a12bdef6479b0e34ed6c1690a330fbf8d
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-11-17 04:38:08 +0700

    feat: init Education page and bind to router

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Helmet } from "react-helmet";
 import Home from './pages/Home';
+import Education from './pages/Education';
+import NavBar from './components/NavBar';
 
 const App = () => {
   return (
-    <div className="flex flex-col justify-between min-h-screen">
+    <div className="flex flex-col min-h-screen">
       <Helmet titleTemplate="Covidfo | %s" defaultTitle="Covidfo">
         <meta name="description" content="Covidfo is COVID-19 Information Tracker Application" />
       </Helmet>
       <BrowserRouter>
+      <NavBar />
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route path="/edu" component={Education} />
         </Switch>
       </BrowserRouter>
     </div>
