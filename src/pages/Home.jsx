5c7f0455efcd34587a44e744481a5e1947bad49c
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-11-17 01:26:10 +0700

    feat: init home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from '../components/NavBar';
+
+const Home = () => {
+  return (
+    <div>
+      <NavBar />
+      <div className="w-screen h-screen"></div>
+    </div>
+  );
+};
+
+export default Home;
