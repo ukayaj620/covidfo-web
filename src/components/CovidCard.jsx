a8d0fc37e99ef1cbe9284d15921a0b6346fef1e7
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-11-17 06:54:50 +0700

    feat: create CovidCard component

---
+++ src/components/CovidCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import image from '../helpers/ImageLoader';
+import { parseNumber } from '../helpers/Functions';
+
+const CovidCard = ({ label, accumulate, today, color, icon }) => {
+
+  return (
+    <div className={`flex w-3/4 lg:w-1/4 flex-row items-center justify-between px-4 py-2 m-4 rounded-xl shadow-lg bg-${color}`}>
+      <div className="flex flex-col w-3/4">
+        <h3 className="font-header font-bold text-305xl text-white">{parseNumber(accumulate)}</h3>
+        <h4>{label}</h4>
+        <h4>{today}</h4>
+      </div>
+      <img className="ml-4 w-1/4" src={image.load(icon)} />
+    </div>
+  );
+};
+
+export default CovidCard;
