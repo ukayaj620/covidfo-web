ad6ef8daad5b3e5d8603a45ee98985daa979a354
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-11-17 01:26:21 +0700

    feat: add Router and bind Home page to router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Helmet } from "react-helmet";
+import Home from './pages/Home';
 
 const App = () => {
   return (
-    <div className="flex flex-col justify-center items-center">
-      <p>Hello</p>
-      <p>React and Tailwind</p>
+    <div className="flex flex-col justify-between min-h-screen">
+      <Helmet titleTemplate="Covidfo | %s" defaultTitle="Covidfo">
+        <meta name="description" content="Covidfo is COVID-19 Information Tracker Application" />
+      </Helmet>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
