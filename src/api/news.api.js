f767e3ff80a01edd74de4629c44319124dc18621
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-11-17 23:49:12 +0700

    feat: fetch news API

--- src/api/news.api.js
+++ src/api/news.api.js
@@ -4,6 +4,7 @@ import key from '../config/APIKey';
 export const getLatestHealthInfo = async () => {
   try {
     const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=id&category=health&apiKey=${key.APIKEY}`)
+    return response;
   } catch (e) {
     return e;
   }
