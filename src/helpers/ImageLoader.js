72d6ae1479eb6d81e59c6db40786a372df4dd75d
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-11-21 17:50:26 +0700

    feat: add images and load them

--- src/helpers/ImageLoader.js
+++ src/helpers/ImageLoader.js
@@ -19,6 +19,12 @@ export default {
         return require('../assets/icons/healthy.svg').default;
       case 'aid':
         return require('../assets/icons/aid.svg').default;
+      case 'social_distancing':
+        return require('../assets/social_distancing.png').default;
+      case 'wash_hand':
+        return require('../assets/wash_hand.png').default;
+      case 'wear_mask':
+        return require('../assets/wear_mask.png').default;
       default:
         return require('../assets/icons/covidfo-icon.svg').default;
     }
