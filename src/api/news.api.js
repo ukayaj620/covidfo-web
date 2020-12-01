ac3572e3b96cc1d8cabc8d3b83cef3e318b32b8d
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-12-01 15:06:51 +0700

    feat: get news from US and change description

--- src/api/news.api.js
+++ src/api/news.api.js
@@ -3,7 +3,7 @@ import { APIKEY } from '../config/APIKey';
 
 export const getLatestHealthInfo = async () => {
   try {
-    const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=id&category=health&apiKey=${APIKEY}`)
+    const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=health&apiKey=${APIKEY}`)
     return response;
   } catch (e) {
     return e;
