da37d6014f56f96ca293e5a3178eb5eb59e578a7
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-12-01 23:07:25 +0700

    feat: addrefresh feature

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
-import * as World from '../api/worlds.api';
-import * as News from '../api/news.api';
+import * as WorldAPI from '../api/worlds.api';
+import * as NewsAPI from '../api/news.api';
 import load from '../helpers/ImageLoader';
 import NavBar from '../components/NavBar';
 import Loading from '../components/Loading';
@@ -9,7 +9,6 @@ import CovidCard from '../components/Card/CovidCard';
 import { parseNumber } from '../helpers/Functions';
 import Separator from '../components/Separator';
 import NewsCard from '../components/Card/NewsCard';
-import FadeIn from '../components/FadeIn';
 import Footer from '../components/Footer';
 
 const HomeBanner = () => {
@@ -18,33 +17,31 @@ const HomeBanner = () => {
   const _navigateTo = (path) => _history.push(path);
 
   return (
-    <FadeIn direction="left">
-      <div className="flex lg:flex-row flex-col items-start justify-between bg-accent px-8">
-        <div className="flex flex-col lg:mr-4 w-full lg:w-2/3 items-center lg:items-start justify-between mt-4 pt-4">
-          <h1 className="dark leading-snug text-center lg:text-left">
-            AYO! Bersama Putuskan Rantai Penyebaran
-            <span className="text-danger"> COVID-19</span>
-          </h1>
-          <h5 className="my-4 text-center lg:text-justify">
-            Tidak pernah lebih jelas bahwa kita semua adalah warga dunia,
-            dan kita semua memiliki peran. Ketegangan novel
-            coronavirus, secara resmi telah mencapai proporsi pandemi. 
-          </h5>
-          <button className="btn" onClick={ () => _navigateTo('/edu') }>
-            <h4>Pelajari Lebih Lanjut</h4>
-          </button>
-        </div>
-        <img
-          className="hidden lg:block w-1/3"
-          src={load('war')}
-          alt="War againts COVID-19"
-        />
+    <div className="flex lg:flex-row flex-col items-start justify-between bg-accent px-8">
+      <div className="flex flex-col lg:mr-4 w-full lg:w-2/3 items-center lg:items-start justify-between mt-4 pt-4">
+        <h1 className="dark leading-snug text-center lg:text-left">
+          AYO! Bersama Putuskan Rantai Penyebaran
+          <span className="text-danger"> COVID-19</span>
+        </h1>
+        <h5 className="my-4 text-center lg:text-justify">
+          Tidak pernah lebih jelas bahwa kita semua adalah warga dunia,
+          dan kita semua memiliki peran. Ketegangan novel
+          coronavirus, secara resmi telah mencapai proporsi pandemi. 
+        </h5>
+        <button className="btn" onClick={ () => _navigateTo('/edu') }>
+          <h4>Pelajari Lebih Lanjut</h4>
+        </button>
       </div>
-    </FadeIn>
+      <img
+        className="hidden lg:block w-1/3"
+        src={load('war')}
+        alt="War againts COVID-19"
+      />
+    </div>
   );
 };
 
-const HomeWorldCases = ({ worldData }) => {
+const HomeWorldCases = ({ worldData, onRefresh }) => {
 
   const WorldCasesData = [
     {
@@ -79,7 +76,15 @@ const HomeWorldCases = ({ worldData }) => {
 
   return (
     <div className="w-full flex flex-col items-center justify-center px-4">
-      <h2 className="dark text-center">Kasus COVID-19 Dunia Terkini</h2>
+      <div className="w-full flex flex-col lg:flex-row items-center justify-center">
+        <h2 className="dark text-center">Kasus COVID-19 Dunia Terkini</h2>
+        <img
+          onClick={onRefresh}
+          src={load('refresh')} 
+          className="w-10 ml-0 lg:ml-4 bg-white rounded-full shadow-lg p-2 my-2 lg:my-0"
+          alt={'click to refresh page'}
+        />
+      </div>
       <h4 className="dark text-center">Update Terakhir: {(new Date(worldData.updated)).toString()}</h4>
       <div className="w-full flex flex-col lg:flex-row items-center mt-8">
         {WorldCasesData.map(({ label, today, accumulate, color, icon }, index) => (
@@ -97,21 +102,40 @@ const HomeWorldCases = ({ worldData }) => {
   );
 };
 
-const HomeNewsCases = ({ newsData }) => {
+const HomeNewsCases = ({ newsData, onRefresh }) => {
+  const _history = useHistory();
+
+  const _navigateTo = (path) => _history.push(path);
+
   return (
     <div className="w-full flex flex-col items-center px-4 justify-center">
-      <h2 className="dark text-center">Berita COVID-19 Indonesia Terkini</h2>
+      <div className="w-full flex flex-col lg:flex-row items-center justify-center">
+        <h2 className="dark text-center">Berita COVID-19 Indonesia Terkini</h2>
+        <img
+          onClick={onRefresh}
+          src={load('refresh')} 
+          className="w-10 ml-0 lg:ml-4 bg-white rounded-full shadow-lg p-2 my-2 lg:my-0"
+          alt={'click to refresh page'}
+        />
+      </div>
       <div className="w-full flex flex-col lg:flex-row items-center mt-8">
         {newsData.slice(0, 4).map(({ title, pubDate, link, enclosure }, index) => (
-          <NewsCard
-            key={`#key-${title}-${index}`}
-            urlToImage={enclosure[0].$.url}
-            source={`Satgas Covid-19 Indonesia`}
-            title={title[0]}
-            url={link[0]}
-            publishedAt={pubDate[0]}
-          />
+          <div className="flex flex-wrap w-4/5 lg:w-1/4">
+            <NewsCard
+              key={`#key-${title}-${index}`}
+              urlToImage={enclosure[0].$.url}
+              source={`Satgas Covid-19 Indonesia`}
+              title={title[0]}
+              url={link[0]}
+              publishedAt={pubDate[0]}
+            />
+          </div>
         ))}
+      </div>
+      <div className="w-full flex flex-col lg:flex-row items-center justify-center mt-4">
+        <button className="btn" onClick={ () => _navigateTo('/news') }>
+          <h4>Baca Lebih Lanjut</h4>
+        </button>
       </div>
     </div>
   );
@@ -119,19 +143,43 @@ const HomeNewsCases = ({ newsData }) => {
 
 const Home = () => {
   const [_isLoading, _setIsLoading] = useState(true);
+  const [_refreshNews, _setRefreshNews] = useState(false);
+  const [_refreshCases, _setRefreshCases] = useState(false);
   const [_worldData, _setWorldData] = useState({});
   const [_newsData, _setNewsData] = useState([]);
   const [_class, _setClass] = useState('opacity-0');
 
+  const _onRefreshCases = () => {
+    _setRefreshCases(true);
+    _setIsLoading(true);
+  }
+
+  const _onRefreshNews = () => {
+    _setRefreshNews(true);
+    _setIsLoading(true);
+  }
+
   useEffect(() => {
     const bootstrapAsync = async () => {
-      const responseWorldData = await World.getWorldBasicInfo();
-      const responseNewsData = await News.getLatestHealthInfo();
+      const responseWorldData = await WorldAPI.getWorldBasicInfo();
       _setWorldData(responseWorldData.data); 
+    }
+    bootstrapAsync().then(() => {
+      _setIsLoading(false);
+      _setRefreshCases(false);
+    });
+  }, [_refreshCases]);
+
+  useEffect(() => {
+    const bootstrapAsync = async () => {
+      const responseNewsData = await NewsAPI.getLatestHealthInfo();
       _setNewsData(responseNewsData);
     }
-    bootstrapAsync().then(() => _setIsLoading(false));
-  }, []);
+    bootstrapAsync().then(() => {
+      _setIsLoading(false);
+      _setRefreshNews(false);
+    });
+  }, [_refreshNews]);
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -146,9 +194,9 @@ const Home = () => {
       <div className="flex flex-col w-full items-center justify-center">
         <HomeBanner />
         <Separator />
-        <HomeWorldCases worldData={_worldData} />
+        <HomeWorldCases worldData={_worldData} onRefresh={_onRefreshCases} />
         <Separator />
-        <HomeNewsCases newsData={_newsData} />
+        <HomeNewsCases newsData={_newsData} onRefresh={_onRefreshNews} />
         <Separator />
       </div>
       <Footer />
