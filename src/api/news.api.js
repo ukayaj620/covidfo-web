320acf0c6383950d50e8e2615b94519df901f971
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-12-01 17:55:48 +0700

    feat: change domain to get covid info

--- src/api/news.api.js
+++ src/api/news.api.js
@@ -1,10 +1,31 @@
 import axios from 'axios';
-import { APIKEY } from '../config/APIKey';
+import { parseString } from 'xml2js';
 
 export const getLatestHealthInfo = async () => {
   try {
-    const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=health&apiKey=${APIKEY}`)
-    return response;
+    const response = await axios.get(
+      `https://cors-anywhere.herokuapp.com/https://covid19.go.id/feed/berita`,
+      {
+        headers: new Headers({
+          Accept: "text/html",
+          "content-type": "application/x-www-form-urlencoded",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET, POST, PUT",
+          "Access-Control-Allow-Headers": "Content-Type",
+        }),
+        mode: "no-cors",
+      });
+
+    let result = null;
+    
+    parseString(response.data, (err, res) => {
+      if (err) {
+        throw new Error("Something wrong happened");
+      } else {
+        result = res.rss.channel[0].item;
+      }
+    });
+    return result;
   } catch (e) {
     return e;
   }
